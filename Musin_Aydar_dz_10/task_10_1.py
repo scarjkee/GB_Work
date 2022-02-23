d11057cef666a3fca32b3a22de5934a65ec5238b
scarjkee <frigtfullscar@mail.ru>
2022-02-23 19:55:24 +0500

    task_10_1_finish_10

--- Musin_Aydar_dz_10/task_10_1.py
+++ Musin_Aydar_dz_10/task_10_1.py
@@ -4,47 +4,47 @@ from numpy import sum
 
 class Matrix:
 
-    def reset_str(func):
-        def wrapper(*args):
-            result = func(*args)
-            res = ''
-            for el in result:
-                res += f'|'
-                for i in el:
-                    res += f' {i}'
-                res += f' |\n'
-            return res
-        return wrapper
-
-
-    @classmethod
-    def __set_len(cls, matrix):
-        st = set()
-        for i in matrix:
-            st.add(len(i))
-        return len(st) < 2
+    # @classmethod
+    # def __set_len(cls, matrix):
+    #     st = set()
+    #     for i in matrix:
+    #         st.add(len(i))
+    #     return len(st) < 2
 
 
     def __init__(self, matrix: List[List[int]]):
-        if type(matrix) == list and len(matrix) > 1 and Matrix.__set_len(matrix):
+        if isinstance(matrix, list) and isinstance(matrix[0], list) and all(map(lambda l: len(matrix[0]) == len(l), matrix)):
             self.matrix = matrix
         else:
             raise ValueError('fail initialization matrix')
 
 
-    @reset_str
+    # @reset_str
     def __str__(self):
-        return self.matrix
+        res = ''
+        for el in self.matrix:
+            res += f'|'
+            for i in el:
+                res += f' {i}'
+            res += f' |\n'
+        return res
 
-    @reset_str
+    # @reset_str
     def __add__(self, other):
-        sum_matrix = sum([self.matrix, other.matrix], axis=0)
+        sum_matrix = ''
+        for i, j in zip(self.matrix, other.matrix):
+            el = ''
+            sum_matrix += f'|'
+            for el1 in zip(i, j):
+                el = sum(el1)
+                sum_matrix += f' {el}'
+            sum_matrix += f' |\n'
         return sum_matrix
 
 
 if __name__ == '__main__':
-    first_matrix = Matrix([[1, 2], [3, 4], [5, 6]])
-    second_matrix = Matrix([[6, 5], [4, 3], [2, 1]])
+    first_matrix = Matrix([[1, 2, 5], [3, 4, 5], [5, 6, 5]])
+    second_matrix = Matrix([[6, 5, 5], [4, 3, 5], [2, 1, 5]])
     print(first_matrix)
     """
     | 1 2 |
